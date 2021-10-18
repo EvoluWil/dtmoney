f4f58f1e1119944f8b2a481d36790de8685cf7b7
Willian-Rodrigues <willian.rsilva1994@gmail.com>
2021-10-18 07:59:12 -0300

    fix bug in initial DB

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,32 @@ import { App } from "./App";
 
 createServer({
   models: { transaction: Model },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance Landing Page",
+          type: "deposit",
+          category: "Web Development",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Rent",
+          type: "withdrawal",
+          category: "House",
+          amount: 1100,
+          createdAt: new Date("2021-02-14 11:00:00"),
+        },
+      ],
+    });
+  },
   routes() {
     this.namespace = "api";
     this.get("/transactions", () => {
-      return this.schema.all("transactions");
+      return this.schema.all("transaction");
     });
     this.post("/transactions", (schema, request) => {
       const data = JSON.parse(request.requestBody);
